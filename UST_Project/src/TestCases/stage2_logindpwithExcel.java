54375d5759906dd104a6133819b902e7963e9424
balamurthy68 <43419549+balamurthy68@users.noreply.github.com>
2024-05-02 10:47:23 +0530

    Add files via upload

---
+++ UST_Project/src/TestCases/stage2_logindpwithExcel.java
@@ -0,0 +1,61 @@
+package testNGtestcases;
+import org.testng.annotations.Test;
+
+import utils.ExcelApiTest;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+
+public class stage2_logindpwithExcel {
+  
+	boolean res;
+	String xlFilePath = "d:\\Test.xlsx";
+	String sheetName = "Sheet1";
+	ExcelApiTest eat = null;
+	
+	
+	@DataProvider
+	  public Object[][] dp(){
+	    return new Object[][] {
+	    	Object[][] dataexcel = readfromexcel(xlFilePath, sheetName);
+	        return dataexcel;
+	    }
+}	
+
+	
+  @Test(dataProvider = "dp")
+  public void login(String u, String p) {
+		
+		System.out.println("Username:" + u + " Password " + p);
+  
+	res = u.equals("tomsmith") ? true : false;
+	
+	Assert.assertEquals(true,res);	
+}
+
+	
+  
+  
+  
+public Object[][] readfromexcel(String xlFilePath, String sheetName) throws Exception
+{
+    Object[][] excelData = null;
+    eat = new ExcelApiTest(xlFilePath);
+    int rows = eat.getRowCount(sheetName);
+    int columns = eat.getColumnCount(sheetName);
+             
+    excelData = new Object[rows-1][columns];
+     
+    for(int i=1; i<rows; i++)
+    {
+        for(int j=0; j<columns; j++)
+        {
+            excelData[i-1][j] = eat.getCellData(sheetName, j, i);
+        }
+         
+    }
+    return excelData;
+}
+
+}
+
